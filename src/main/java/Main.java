fc94fc37ac531b94fa5b4ee4e8417e1e8b4a3016
Fernando <fernandopfennig@gmail.com>
2018-11-26 18:31:30 +0100

    Changed if blocks to switch case

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws UndefinedMathException {
         CalcFunctions calc = new CalcFunctions();
 
         int num1 = 0;
@@ -15,26 +15,30 @@ public class Main {
 
         Scanner scanObject = new Scanner(System.in);
 
+        System.out.println("What do you want to do?\nThere are following functions:\nadd\nsubtract\nmultiply\ndivide");
+        function = scanObject.next();
         System.out.println("Enter your first number:");
         num1 = scanObject.nextInt();
         System.out.println("Enter your second number:");
         num2 = scanObject.nextInt();
-        System.out.println("What do you want to do?\nThere are following functions:\nadd\nsubtract\nmultiply\ndivide");
-        function = scanObject.next();
 
 
-        if(function.equals("add")){
-            result = calc.add(num1,num2);
-        } else if(function.equals("subtract")){
-            result = calc.subtract(num1,num2);
-        } else if(function.equals("multiply")){
-            result = calc.multiply(num1,num2);
-        } else if(function.equals("divide")){
-            try {
-                result = calc.divide(num1, num2);
-            } catch (UndefinedMathException e) {
-                e.printStackTrace();
-            }
+
+//        if(function.equals("add")){
+//            result = calc.add(num1,num2);
+//        } else if(function.equals("subtract")){
+//            result = calc.subtract(num1,num2);
+//        } else if(function.equals("multiply")){
+//            result = calc.multiply(num1,num2);
+//        } else if(function.equals("divide")){
+//                result = calc.divide(num1, num2);
+//        }
+
+        switch (function) {
+            case "add": result = calc.add(num1,num2); break;
+            case "subtract": result = calc.subtract(num1,num2); break;
+            case "multiply": result = calc.multiply(num1,num2); break;
+            case "divide": result = calc.divide(num1,num2); break;
         }
         System.out.println("The result is "+result);
     }
